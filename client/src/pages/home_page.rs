f02e19ca05d3d3c7b25f7362696d240d46749a98
Chuck Okoroji <dandychux@gmail.com>
2024-03-29 16:50:24 -0400

    removed github packages repo push from workflow

--- client/src/pages/home_page.rs
+++ client/src/pages/home_page.rs
@@ -3,10 +3,11 @@ use yew::prelude::*;
 #[function_component(HomePage)]
 pub fn home_page() -> Html {
     html! {
-        <section class="h-full pt-20">
-            <div class="max-w-4xl mx-auto rounded-md h-[20rem] flex justify-center items-center">
-                <p class="text-3xl font-semibold">{"Welcome to Rusty Melody"}</p>
-            </div>
+        <section class="h-full">
+        <div>
+            <h3 class="my-0">{"Similar Songs"}</h3>
+            <small>{"Based on your search"}</small>
+        </div>
         </section>
     }
 }
