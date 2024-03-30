0d702cfb986322168b57cc3c3db6aced4c430308
Chuck Okoroji <dandychux@gmail.com>
2024-03-30 16:55:30 -0400

    added grid to homepage; updated server Dockerfile

--- client/src/pages/home_page.rs
+++ client/src/pages/home_page.rs
@@ -4,10 +4,14 @@ use yew::prelude::*;
 pub fn home_page() -> Html {
     html! {
         <section class="h-full">
-        <div>
-            <h3 class="my-0">{"Similar Songs"}</h3>
-            <small>{"Based on your search"}</small>
-        </div>
+            <div>
+                <h3 class="my-0 mb-2">{"Similar Songs"}</h3>
+                <small>{"Based on your search"}</small>
+            </div>
+
+            <div class="grid grid-cols-2 auto-cols-auto">
+                // Loop through results and display a SongCard for each
+            </div>
         </section>
     }
 }
